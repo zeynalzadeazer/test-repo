c90222d166464e840f66d4db69061f028678181c
azeynalzade <azer.zeynalzade@pashabank.az>
2019-07-18 12:30:49 +0400

    all changes

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -3,10 +3,12 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication''
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiDemoApplication.class, args);
+
+		System.out.println("develop branch") ;
+
 	}
-}
